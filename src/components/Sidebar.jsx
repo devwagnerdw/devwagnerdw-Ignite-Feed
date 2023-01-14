0154538ec4f4ff497436ffcabefd31a2173bbd57
vagne <vagnerdw1@gmail.com>
2023-01-13 22:27:59 -0300

    Estrutura da aplicação - Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,6 @@
+import { PencilLine} from 'phosphor-react'
+
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar(){
@@ -8,12 +11,16 @@ export function Sidebar(){
       />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/devwagnerdw.png" />
         <strong> Vagner alves</strong>
         <span>Web developer</span>
       </div>
 
         <footer>
-          <a href="#"> editar seu perfil</a>
+          
+          <a href="#"> 
+          <PencilLine  size={20}/>
+          editar seu perfil</a>
         </footer>
     </aside>
   );
