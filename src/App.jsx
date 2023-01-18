a81a13d35b5e1674fe12cac51d12efc008d089aa
vagne <vagnerdw1@gmail.com>
2023-01-18 17:58:26 -0300

    Propriedades do Post/ Ajustando horarios e Posts

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,38 @@ import styles from './App.module.css';
 
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/devwagnerdw.png',
+      name: 'Vagne alves',
+      role: 'Develop web'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-01-10 20:00:00'),
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/franciscovinicios.png',
+      name: 'Vinicius Alves',
+      role: 'Develop web'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera  👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-01-18 17:00:00'),
+  },
+];
+
 export function App (){
   return (
  <div> 
@@ -13,15 +45,17 @@ export function App (){
    <div className={styles.wrapper}>
    <Sidebar />
     <main>
-    <Post
-       author="vagne alves" 
-       content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam, porro iure. Officia eveniet magnam facilis vero doloremque. Fuga nulla, pariatur cum cumque molestias aliquam iusto commodi odio perferendis, adipisci consequuntur."  
-     />
+      {posts.map(post => {
+       return (
+          <Post
+          author={post.author}
+          content={post.content}
+          publishedAt={post.publishedAt}
 
-     <Post 
-     author=" vagner sena"
-     content="alvesdesousa"
-     />
+
+          />
+        )
+      })}
     </main>
    </div>
  </div>)
