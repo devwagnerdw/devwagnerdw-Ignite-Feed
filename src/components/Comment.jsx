fc1553ec5338b1f86173fe3d97729d5090d2d096
vagne <vagnerdw1@gmail.com>
2023-01-23 20:41:13 -0300

    likes nos comentários

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,12 +2,22 @@ import styles from './Comment.module.css';
 
 import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
 export function Comment({content,onDeleteComment}) {
+const[likeCount, setLikeCount]= useState(0);
+
+
+
   function handleDeleteComment(){
     onDeleteComment(content)
-
   }
+
+  function handleLikeComment(){
+    setLikeCount(likeCount + 1);
+  };
+
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/franciscovinicios.png" />
@@ -29,9 +39,9 @@ export function Comment({content,onDeleteComment}) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>24</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
